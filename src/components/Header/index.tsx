9d2a537d86c238e0fb6f82ad60a13919b0b8a66a
Mariana <mariana.iwaivk@gmail.com>
2025-12-10 12:10:23 -0300

    feat(header): add Header component with index.tsx, types and styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import Credit from '../../assets/svg/credit.svg';
+import UserImg from '../../assets/svg/userImg.svg';
+import styles from './Header.module.css';
+import type { HeaderProps } from './Header.types';
+
+export function Header({ name, email }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={Credit} alt="credito" />
+        <h1>Finance Tracker</h1>
+      </div>
+
+      <div className={styles.user}>
+        <div className={styles.details}>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+        <img src={UserImg} className={styles.userImg} alt="user" />
+      </div>
+    </header>
+  );
+}
