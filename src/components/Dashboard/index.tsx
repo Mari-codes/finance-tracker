2e0f7f6fd273f0cbaec59d7515af8fbb706b0f53
Mariana <mariana.iwaivk@gmail.com>
2025-12-11 16:22:40 -0300

    feat(styles): add responsive media queries to all components

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -14,21 +14,21 @@ export function Dashboard() {
   const [transactions, setTransactions] = useState<Transaction[]>([
     {
       name: 'Compra de Alimentos',
-      date: '15/08/2023',
+      date: '01/07/2025',
       category: 'Alimentação',
       amount: 50.0,
       type: '-',
     },
     {
       name: 'Salário',
-      date: '01/08/2023',
+      date: '01/08/2025',
       category: 'Renda',
       amount: 1000.0,
       type: '+',
     },
     {
       name: 'Conta de Luz',
-      date: '10/08/2023',
+      date: '10/08/2025',
       category: 'Despesas',
       amount: 80.0,
       type: '-',
